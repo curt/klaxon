fa8c4398e47522fc2790cb5b259084b66769f124
Curt <curt@users.noreply.github.com>
2023-04-29 09:04:32 -0700

    update subscription forms

--- lib/klaxon_web/views/subscription_view.ex
+++ lib/klaxon_web/views/subscription_view.ex
@@ -1,3 +1,11 @@
 defmodule KlaxonWeb.SubscriptionView do
   use KlaxonWeb, :view
+
+  @schedule_keywords [hourly: "Hourly", daily: "Daily", weekly: "Weekly", none: "Suspend"]
+
+  defp schedule_options() do
+    Enum.map(Ecto.Enum.values(Klaxon.Syndication.Subscription, :schedule), fn x ->
+      {@schedule_keywords[x], x}
+    end)
+  end
 end
