58d7bd0401e6779034cc039b342fa81750bb5f2b
Curt Gilman <curt@gilm.net>
2023-08-15 12:47:36 -0500

    attachment caption html, use inner

--- priv/repo/migrations/20230815163708_alter_attachments_populate_caption_html.exs
+++ priv/repo/migrations/20230815163708_alter_attachments_populate_caption_html.exs
@@ -5,13 +5,14 @@ defmodule Klaxon.Repo.Migrations.AlterAttachmentsPopulateCaptionHtml do
   def up do
     for attachment <- Klaxon.Repo.all(Klaxon.Contents.Attachment) do
       if attachment.caption do
-        Klaxon.Contents.Attachment.changeset(attachment, %{caption_html: Earmark.as_html!(attachment.caption)})
+        Klaxon.Contents.Attachment.changeset(attachment, %{
+          caption_html: String.trim(Earmark.as_html!(attachment.caption, inner_html: true))
+        })
         |> Klaxon.Repo.update()
       end
     end
   end
 
   def down do
-
   end
 end
