d01f14c36c8651eb492f91be4944b8bac361686c
Curt Gilman <curt@gilm.net>
2023-08-15 12:06:38 -0500

    display attachment caption html

--- lib/klaxon/contents/attachment.ex
+++ lib/klaxon/contents/attachment.ex
@@ -3,6 +3,7 @@ defmodule Klaxon.Contents.Attachment do
 
   schema "attachments" do
     field :caption, :string
+    field :caption_html, :string
 
     belongs_to :post, Klaxon.Contents.Post, type: EctoBase58
     belongs_to :media, Klaxon.Media.Media, type: EctoBase58
@@ -13,7 +14,13 @@ defmodule Klaxon.Contents.Attachment do
   @doc false
   def changeset(attachment, attrs \\ %{}) do
     attachment
-    |> cast(attrs, [:caption])
+    |> cast(attrs, [:caption, :caption_html])
     |> validate_required([:caption])
+  end
+
+  def apply_caption_html(changeset) do
+    if caption = get_change(changeset, :caption) do
+      put_change(changeset, :caption_html, Earmark.as_html!(caption))
+    end || changeset
   end
 end
