ceb46f240498ecd91a65485b89a83ae080885fbe
Curt Gilman <14070+curt@users.noreply.github.com>
2024-06-04 13:12:04 +0200

    fix: repair invalid tests

--- test/klaxon_web/controllers/subscription_controller_test.exs
+++ test/klaxon_web/controllers/subscription_controller_test.exs
@@ -24,7 +24,8 @@ defmodule KlaxonWeb.SubscriptionControllerTest do
       conn =
         post(
           conn,
-          Routes.subscription_path(conn, :unsubscribe, subscription.id, subscription.key)
+          Routes.subscription_path(conn, :unsubscribe, subscription.id, subscription.key),
+          "List-Unsubscribe": "One-Click"
         )
 
       assert text_response(conn, 200)
@@ -37,10 +38,26 @@ defmodule KlaxonWeb.SubscriptionControllerTest do
       conn =
         post(
           conn,
-          Routes.subscription_path(conn, :unsubscribe, id, key)
+          Routes.subscription_path(conn, :unsubscribe, id, key),
+          "List-Unsubscribe": "One-Click"
         )
 
       assert text_response(conn, 200)
+    end
+
+    test "unsubscribe with good id and key, missing params, bad response", %{
+      conn: conn,
+      subscription: subscription
+    } do
+      assert_raise(
+        Phoenix.ActionClauseError,
+        fn ->
+          post(
+            conn,
+            Routes.subscription_path(conn, :unsubscribe, subscription.id, subscription.key)
+          )
+        end
+      )
     end
 
     test "unsubscribe with get, bad response", %{conn: conn, subscription: subscription} do
