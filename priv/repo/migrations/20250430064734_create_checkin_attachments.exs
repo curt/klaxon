4c1a18cb7c54269a1d288473c386486141f78fc3
Curt Gilman <14070+curt@users.noreply.github.com>
2025-04-30 08:59:10 +0200

    add migration for checkin attachments

---
+++ priv/repo/migrations/20250430064734_create_checkin_attachments.exs
@@ -0,0 +1,17 @@
+defmodule Klaxon.Repo.Migrations.CreateCheckinAttachments do
+  use Ecto.Migration
+
+  def change do
+    create table(:checkin_attachments, primary_key: false) do
+      add :id, :binary_id, primary_key: true
+      add :caption, :string
+      add :checkin_id, references(:places, type: :binary_id, on_delete: :delete_all), null: false
+      add :media_id, references(:media, type: :binary_id, on_delete: :delete_all), null: false
+
+      timestamps(type: :utc_datetime_usec)
+    end
+
+    create index(:checkin_attachments, [:checkin_id])
+    create index(:checkin_attachments, [:media_id])
+  end
+end
