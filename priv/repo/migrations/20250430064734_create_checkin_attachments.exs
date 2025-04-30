1bb240661d8c2341380370262881dd6174c40e1c
Curt Gilman <14070+curt@users.noreply.github.com>
2025-04-30 16:50:05 +0200

    fix foreign key reference in checkin attachments

--- priv/repo/migrations/20250430064734_create_checkin_attachments.exs
+++ priv/repo/migrations/20250430064734_create_checkin_attachments.exs
@@ -5,7 +5,10 @@ defmodule Klaxon.Repo.Migrations.CreateCheckinAttachments do
     create table(:checkin_attachments, primary_key: false) do
       add :id, :binary_id, primary_key: true
       add :caption, :string
-      add :checkin_id, references(:places, type: :binary_id, on_delete: :delete_all), null: false
+
+      add :checkin_id, references(:checkins, type: :binary_id, on_delete: :delete_all),
+        null: false
+
       add :media_id, references(:media, type: :binary_id, on_delete: :delete_all), null: false
 
       timestamps(type: :utc_datetime_usec)
