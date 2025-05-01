1588188897685fe685d0818b39fe380c7e0be059
Curt Gilman <curt@users.noreply.github.com>
2025-05-01 10:51:37 +0200

    display attachments on show checkin page (#64)

--- lib/klaxon_web/views/checkin_view.ex
+++ lib/klaxon_web/views/checkin_view.ex
@@ -1,3 +1,17 @@
 defmodule KlaxonWeb.CheckinView do
   use KlaxonWeb, :view
+
+  def status_action(%{status: status}) do
+    case status do
+      :draft -> "drafted"
+      :published -> "checked in"
+      _ -> "appeared"
+    end
+  end
+
+  def status_date(%{status: status} = checkin) do
+    case status do
+      _ -> checkin.checked_in_at
+    end
+  end
 end
