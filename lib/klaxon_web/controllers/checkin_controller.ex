fde35d30648fc3fac4c4a9aa7278fd8fcb3edaf0
Curt Gilman <curt@users.noreply.github.com>
2025-05-24 16:36:32 -0700

    resolves #56, resolves #57

--- lib/klaxon_web/controllers/checkin_controller.ex
+++ lib/klaxon_web/controllers/checkin_controller.ex
@@ -27,7 +27,7 @@ defmodule KlaxonWeb.CheckinController do
              conn.assigns.current_user,
              place_id
            ) do
-      render(conn, "index.html", place: place, checkins: checkins)
+      render(conn, "index.html", place: place, checkins: checkins, title: "Check-Ins")
     end
   end
 
@@ -45,7 +45,11 @@ defmodule KlaxonWeb.CheckinController do
              place_id,
              id
            ) do
-      render(conn, "show.html", place: place, checkin: checkin)
+      render(conn, "show.html",
+        place: place,
+        checkin: checkin,
+        title: "Check-In at #{place.title}"
+      )
     end
   end
 
