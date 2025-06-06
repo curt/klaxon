88a298e9a1c345c8d69a5144be8ccb2f50cff1d5
Curt Gilman <curt@users.noreply.github.com>
2025-06-05 18:03:08 -0700

    fix missing json codec for aws

--- config/config.exs
+++ config/config.exs
@@ -33,6 +33,7 @@ config :waffle,
   virtual_host: true
 
 config :ex_aws,
+  json_codec: Jason,
   access_key_id: [{:system, "AWS_ACCESS_KEY_ID"}, :instance_role],
   secret_access_key: [{:system, "AWS_SECRET_ACCESS_KEY"}, :instance_role],
   region: [{:system, "AWS_REGION"}, :instance_role]
