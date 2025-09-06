6cf836593f7d514caef1dedb9c9ab17f084f700a
Curt Gilman <curt@users.noreply.github.com>
2025-09-06 09:35:10 +0200

    remove redundant list concat from compilers

--- mix.exs
+++ mix.exs
@@ -7,7 +7,7 @@ defmodule Klaxon.MixProject do
       version: version(),
       elixir: "~> 1.12",
       elixirc_paths: elixirc_paths(Mix.env()),
-      compilers: [] ++ Mix.compilers(),
+      compilers: Mix.compilers(),
       start_permanent: Mix.env() == :prod,
       aliases: aliases(),
       deps: deps()
